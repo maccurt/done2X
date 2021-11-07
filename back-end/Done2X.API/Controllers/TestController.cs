b9f238b2be8173bdc48e9c19157ec71b6cb7a3e4
Michael McGee <dineromike@yahoo.com>
2021-11-07 06:15:44 -0600

    appsetting.json made prod version did not check it

---
+++ back-end/Done2X.API/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Done2X.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet()]
+        public async Task<IEnumerable<string>> Get()
+        {
+            
+            List<string> list;
+            list = new List<string>();
+
+            list.Add("Test Controller");
+
+            var aspnetcoreEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (aspnetcoreEnvironment != null)
+            {
+                list.Add(aspnetcoreEnvironment);
+            }
+
+            return list;
+
+        }
+    }
+}
