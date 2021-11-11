e4c4cf33588c8c5518e1e3d39ccfa18f64a956e5
Michael McGee <dineromike@yahoo.com>
2021-11-11 09:40:44 -0600

    Add ability to go to production, added web config

--- back-end/Done2X.Domain/Goal.cs
+++ back-end/Done2X.Domain/Goal.cs
@@ -6,5 +6,6 @@
         public int ProjectId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string WhatIsDone { get; set; }
     }
 }
--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
   testing: false,
-  allowedList: ['*Done2X.API/api/*'] //FIX THIS
+  API_URL: 'https://api.done2x.com/api/',
+  allowedList: ['*api.done2x.com/api/*'],
+  auth: {
+    domain: 'profitdreamer.auth0.com',
+    clientId: '1GBzlqLA7ch77mINpaqruTnSrXaKbjp2',
+    audience: 'https://api.done2x.com/api',
+    serverUrl: 'http://localhost/Done2X.API/api/', //? Why us this localhost
+    redirectUri: window.location.origin,
+  }
 };
