7b3579d8800e884bc07c9a45602524d9e549f986
Maccurt <dineromike@yahoo.com>
2021-11-22 12:20:03 -0800

    added the basic homepage v1

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { Subscription } from 'rxjs';
+import { faGraduationCap, faFrog, faChalkboardTeacher,faCheckSquare,faGlassCheers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,25 @@ import { Subscription } from 'rxjs';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
+  //Font Awesome Icons
+  learnIcon = faGraduationCap;
+  wartsAndAllIcon = faFrog;
+  experimentLearnIcon = faChalkboardTeacher;
+  goalsTaskIcon = faCheckSquare;
+  celebrateIcon = faGlassCheers;
+
+  //subs$ to dispose
   isAuthenticated$!: Subscription;
+
+
   constructor(private authService: AuthService) { }
 
   public ngOnInit(): void {
     // this.isAuthenticated$ = this.authService.isAuthenticated$.subscribe(() => {
     //   console.log('authenticated in home');
     // })
+
+
 
   }
 
