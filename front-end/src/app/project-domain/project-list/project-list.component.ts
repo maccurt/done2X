5acbdb5232075f0b392972adf076a5e881bf487e
Michael McGee <dineromike@yahoo.com>
2022-01-08 16:06:55 -0600

    add project-list route and showed the basic data on the screen

--- front-end/src/app/project-domain/project-list/project-list.component.ts
+++ front-end/src/app/project-domain/project-list/project-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Project } from '../project.type';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss']
 })
-export class ProjectListComponent implements OnInit {
+export class ProjectListComponent implements OnInit, OnDestroy {
+  routeData$!: Subscription;
+  projectList: Project[] = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.routeData$ = this.route.data.subscribe((data) => {
+      this.projectList = data.projectList;
+    });
   }
 
+  ngOnDestroy(): void {
+    this.routeData$?.unsubscribe();
+  }
 }
